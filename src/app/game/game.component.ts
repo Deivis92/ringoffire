50163251e3ec2f0b4af030128f2f77864d03dec7
Deividas Kondratjevas <deividaskond@gmail.com>
2024-09-18 20:16:06 +0200

    second commit

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  
+  game: Game;
+  constructor() { 
+    this.game = new Game;
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+
+}
